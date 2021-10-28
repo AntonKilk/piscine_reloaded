a5022573a8b4d4836fca1f2183ea8c74d88e9a0f
AntonKilk <ant.kilk@gmail.com>
2021-10-28 07:36:04 +0300

    ex20

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char *dup;
+	int size;
+	int i;
+
+	size = 0;
+	while(src[size])
+		size++;
+	dup = (char*)malloc(sizeof(char) * size + 1);
+	if(dup == 0)
+		return (0);
+	i = 0;
+	while (i < size)
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
